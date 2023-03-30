ded1452bb52dc7f1cf9e52cd5194c1e0961bf179
JuanSebastianOso <plaranda13@gmail.com>
2023-03-30 17:02:42 -0500

    trabajo de android 1

---
+++ app/src/main/java/com/yeison/myapp/colors.java
@@ -0,0 +1,14 @@
+package com.yeison.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class colors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
